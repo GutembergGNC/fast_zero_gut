e5bd3e3eafaaff0e54ad3fe350dba12119f3de58
Guto GNC <gutemberg27@gmail.com>
2025-08-07 14:55:02 -0300

    Aula 01: Criando repositório

---
+++ tests/test_app.py
@@ -0,0 +1,24 @@
+from http import HTTPStatus
+
+from fastapi.testclient import TestClient
+
+from fastapi_zero.app import app
+
+
+def test_root_deve_retornar_ola_mundo():
+    """
+    Esse teste tem 3 etapas (AAA):
+    A: arrange  - Arranjo
+    A: act      - Executa a coista (o SUT)
+    A: assert   - Garanta que A é A
+    """
+
+    """arrange:"""
+    client = TestClient(app)
+
+    """act:"""
+    response = client.get('/')
+
+    """assert:"""
+    assert response.json() == {'message': 'Olá mundo!'}
+    assert response.status_code == HTTPStatus.OK
